a206415f89c31986a4fc8376baf5b03f04b7508c
Dhruv Vashistha <dvas7337@gmail.com>
2025-05-25 11:01:41 +0530

    Delete dv_67_projects.java

--- dv_67_projects.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class dv_67_projects {
-    public static void main(String[] args) {
-        /*
-        all the projects are saved in different package of vs code as dv_67_projects
-         */
-    }
-}
